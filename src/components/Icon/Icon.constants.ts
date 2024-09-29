4cddde58316a4ac2632a62d79393176473ab47a7
Juan Cortes <juancor.dev@gmail.com>
2024-09-29 19:01:28 +0200

    feat: add social media links with icons

--- src/components/Icon/Icon.constants.ts
+++ src/components/Icon/Icon.constants.ts
@@ -1,8 +1,14 @@
+import EmptyIcon from "./assets/EmptyIcon.astro";
+import GithubIcon from "./assets/GithubIcon.astro";
+import LinkedinIcon from "./assets/LinkedinIcon.astro";
 import MonitorIcon from "./assets/MonitorIcon.astro";
 import MoonIcon from "./assets/MoonIcon.astro";
 import SunIcon from "./assets/SunIcon.astro";
 
 export const ICON_BY_NAME = {
+  empty: EmptyIcon,
+  github: GithubIcon,
+  linkedin: LinkedinIcon,
   monitor: MonitorIcon,
   moon: MoonIcon,
   sun: SunIcon,
---
+++ src/constants/social.ts
@@ -0,0 +1,4 @@
+export const SOCIAL_PROFILE = {
+  GITHUB: "https://github.com/core97",
+  LINKEDIN: "https://linkedin.com/in/juan-cortes-a01043151",
+};

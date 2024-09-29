e0ab487af6d78830a01b44a17500d7cff060551e
Juan Cortes <juancor.dev@gmail.com>
2024-09-29 19:00:37 +0200

    feat: create blur-fade animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,19 @@ import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: 'selector',
+  darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "blur-fade": {
+          "0%": { opacity: "0", transform: "translateY(0.75rem)" },
+          "100%": { opacity: "1", transform: "translateY(0rem)" },
+        },
+      },
+      animation: {
+        "blur-fade": "blur-fade 0.7s ease-in-out",
+      },
+    },
   },
   plugins: [typography],
 };
